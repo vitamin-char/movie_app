b600f1d3d0a2d3163a4a31b58451881d2e4d6659
hayoung9000 <vitamin.char@gmail.com>
2019-09-12 08:10:14 +0900

    PropTypes로 prop 유효성 검사

--- src/App.js
+++ src/App.js
@@ -24,8 +24,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {movies.map(movie => {
-          return <Movie title={movie.title} poster={movie.poster} />;
+        {movies.map((movie, index) => {
+          return (
+            <Movie title={movie.title} poster={movie.poster} key={index} />
+          );
         })}
       </div>
     );
