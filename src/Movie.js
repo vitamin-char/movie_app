bfc55b9cecefe81b531871159434a6e3d1ac2f06
hayoung9000 <vitamin.char@gmail.com>
2019-09-16 21:24:31 +0900

    Functional Component로 변경

--- src/Movie.js
+++ src/Movie.js
@@ -1,30 +1,28 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
-class Movie extends Component {
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
-  };
-
-  render() {
-    return (
-      <div>
-        <MoviePoster poster={this.props.poster} />
-        <h1>{this.props.title}</h1>
-      </div>
-    );
-  }
+function Movie({title, poster}){
+  return (
+    <div>
+      <MoviePoster poster={poster} />
+      <h1>{title}</h1>
+    </div>
+  )
 }
 
-class MoviePoster extends Component {
-  static propTypes = {
-    poster: PropTypes.string.isRequired
-  };
+function MoviePoster({poster}){
+  return(
+    <img src={poster} />
+  )
+}
 
-  render() {
-    return <img src={this.props.poster} />;
-  }
+Movie.propTypes = {
+  title: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired
+}
+
+MoviePoster.prototype = {
+  poster: PropTypes.string.isRequired
 }
 export default Movie;
